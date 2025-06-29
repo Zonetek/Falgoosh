1dcadc6a1ddc874cb170ab02cfa015fd555302fd
Mobinamousavii <mobinamsv6@gmail.com>
2025-06-29 17:37:43 +0330

    feat(scan): Add Scan model

--- backend/api_applications/scan/models.py
+++ backend/api_applications/scan/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Scan(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('running', 'Running'),
+        ('done', 'Done'),
+        ('failed', 'Failed'),
+    ]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='scans')
+    target = models.TextField(max_length=250)
+    ports = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=20 , choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    result_reference = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"Scan #{self.id} - {self.target}"
