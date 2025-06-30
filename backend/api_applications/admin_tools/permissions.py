809139e540c185b176bafda108c4d10acf69e077
Mobinamousavii <mobinamsv6@gmail.com>
2025-06-30 16:44:39 +0330

    feat(admin): Add Hasgroup permission.

---
+++ backend/api_applications/admin_tools/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+def HasGroup(group_name):
+    class GroupPermission(BasePermission):
+        def has_permission(self, request, view):
+            return (request.user and
+            request.user.is_authenticated and
+            request.user.groups.filter(name =group_name).exists())
+    return GroupPermission
+        
