3fd9b8076d3faf86979cbc44f65eefcb4ef030c7
Mobinamousavii <mobinamsv6@gmail.com>
2025-06-30 13:57:41 +0330

    feat(admin): add management command for creating admin groups and permissions

--- backend/api_applications/accounts/managements/commands/create_admin_groups.py
+++
@@ -1,4 +0,0 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import Group, Permission
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth import get_user_model
