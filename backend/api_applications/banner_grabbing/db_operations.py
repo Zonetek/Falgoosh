edcd8d75458ebc145aacc4751886afc017e0ad76
parisa-arabi <parisa015030@gmail.com>
2025-07-12 16:53:08 +0330

    chore: add vulnerability and fix threading
    - Imported vulnerability module.
    - Updated main function to run enrichment, banner, and vulnerability updates in separate threads.
    - Added scheduled NVD data update using schedule library.

--- backend/api_applications/banner_grabbing/db_operations.py
+++ backend/api_applications/banner_grabbing/db_operations.py
@@ -2,7 +2,7 @@ import logging
 
 from shared_libs import monogo_connections
 
-from banner_grabbing import banner_grabber, enrichment
+from . import banner_grabber, enrichment, vulnerability
 
 
 def update_enrichment():
@@ -41,4 +41,22 @@ def update_banners():
         logging.info(f"getting SERVICE TYPE of : {i}")
         db.scan_results.update_one(
             {"_id": i["_id"]}, {"$set": {"service_type": banner}}
+        )
+
+def update_vulnerability():
+    try:
+        db = monogo_connections.connect_monogo()
+        results = list(db.scan_results.find({
+        "vulnerability": {"$exists": False},
+        "service_type": {"$exists": True, "$ne": None}
+    }))
+    except Exception as e:
+        logging.info(f"Operation do not complete : {e}")
+        return None
+    for i in results:
+        vul = vulnerability.get_vul(i["service_type"])
+        logging.info(f"Updating vuls, result is: {vul}")
+        logging.info(f"getting vuls of : {i}")
+        db.scan_results.update_one(
+            {"_id": i["_id"]}, {"$set": {"vulnerability": vul}}
         )
