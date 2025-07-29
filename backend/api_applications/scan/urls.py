3d40f0572ee3fd12a8f4e262fbceb907e2ce6517
Mobinamousavii <mobinamsv6@gmail.com>
2025-07-30 01:13:18 +0330

    feat(scan): add URL routes for UserScansView and UserScanHistoryView

--- backend/api_applications/scan/urls.py
+++ backend/api_applications/scan/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import SearchIPView
+from .views import SearchIPView, UserScansView, UserScanHistoryView
 
 urlpatterns = [
    path("search/", SearchIPView.as_view(), name="search_ip"),
+   path('scans/', UserScansView.as_view(), name='user_scans'),
+    path('scans/<int:scan_id>/history/', UserScanHistoryView.as_view(), name='user_scan_history'),
 ]
