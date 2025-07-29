cf9c2f06a090b2f811cac7918fa09c000148b5bb
Mobinamousavii <mobinamsv6@gmail.com>
2025-07-29 23:54:16 +0330

    feat(scan): add scan history logging for authenticated users

--- backend/api_applications/scan/views.py
+++ backend/api_applications/scan/views.py
@@ -1,11 +1,12 @@
 import ipaddress
 import logging
-
+from django.utils import timezone
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api_applications.shared_libs.mongo_fetch_result import fetch_by_ip
+from api_applications.shared_models.models.scan import ScanHistory, Scan
 
 logger = logging.getLogger(__name__)
 
@@ -38,6 +39,54 @@ class SearchIPView(APIView):
                 {"error": "Internal server error while processing request"},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
+        
+        if request.user.is_authenticated:
+            scan, created = Scan.objects.get_or_create(
+                user = request.user,
+                target_ip = ip,
+                defaults={
+                    "status" : "pending",
+                    "created_at": timezone.now(),
+                }
+            )
+            if created:
+                ScanHistory.objects.create(
+                    user=request.user,
+                    scan=scan,
+                    action="created",
+                    details={"note": "Scan record created"},
+                )
+
+            if ip_data:
+                scan.status = "running"
+                scan.started_at = timezone.now()
+                scan.save(update_fields=["status", "started_at", "updated_at"])
+                ScanHistory.objects.create(
+                        user=request.user,
+                        scan=scan,
+                        action="running",
+                        details={},
+                    )
+                
+                scan.status = "completed"
+                scan.completed_at = timezone.now()
+                scan.save(update_fields=["status", "completed_at", "updated_at"])
+                ScanHistory.objects.create(
+                    user=request.user,
+                    scan=scan,
+                    action="completed",
+                    details=ip_data,
+                )
+
+            else:
+                scan.status = "failed"
+                scan.save(update_fields=["status", "updated_at"])
+                ScanHistory.objects.create(
+                    user=request.user,
+                    scan=scan,
+                    action="no_data",
+                    details={"ip": ip},
+                )
 
         if ip_data:
             return Response(ip_data)
@@ -46,3 +95,5 @@ class SearchIPView(APIView):
                 {"message": f"No data found for IP: {ip}"},
                 status=status.HTTP_404_NOT_FOUND,
             )
+
+        
