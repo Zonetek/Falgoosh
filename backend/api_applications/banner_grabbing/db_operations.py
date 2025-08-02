7d94251cf7c4523b4a1ba814c08dbfdf2eedb77e
parisa-arabi <parisa015030@gmail.com>
2025-08-02 11:48:59 +0330

    feat(vulnerability): separate vulnerabilities from banner service
    - Extract vulnerabilities logic from banner service into its own service
    - Add Dockerfile for vulnerability service
    - Update docker-compose to include vulnerability service

--- backend/api_applications/banner_grabbing/db_operations.py
+++ backend/api_applications/banner_grabbing/db_operations.py
@@ -2,7 +2,8 @@ import logging
 
 from shared_libs import monogo_connections
 
-from . import banner_grabber, vulnerability
+from . import banner_grabber
+from . import producer
 
 
 def update_banners():
@@ -18,43 +19,19 @@ def update_banners():
             )
         )
         for i in results:
-            banner = banner_grabber.scan_ports_for_banners(i["_id"], i["ports"])
+            banner = banner_grabber.scan_ports_for_banners(
+                i["_id"], i["ports"])
             logging.info(f"Updating banner, result is: {banner}")
             logging.info(f"getting SERVICE TYPE of : {i}")
             db.scan_results.update_one(
                 {"_id": i["_id"]}, {"$set": {"service_type": banner}}
             )
-    
+
+            try:
+                producer.producer_queue(str(banner))
+
+            except Exception as e:
+                logging.error(f"Falid to put banner in queue {e}")
+
     except Exception as e:
         logging.error(f"Operation do not complete in update banner : {e}")
-
-
-def update_vulnerability():
-
-    try:
-        db = monogo_connections.connect_monogo()
-        results = list(
-            db.scan_results.find(
-                {
-                    "vulnerability": {"$exists": False},
-                    "service_type": {"$exists": True, "$ne": None},
-                    "ports": {"$exists": True, "$nin": [[], None]},
-                }
-            )
-        )
-        try:
-
-            for i in results:
-                vul = vulnerability.get_vul(i["service_type"])
-                if vul:
-                    logging.info(f"Updating vuls, result is: {vul}")
-                    logging.info(f"getting vuls of : {i}")
-                    db.scan_results.update_one(
-                        {"_id": i["_id"]}, {"$set": {"vulnerability": vul}}
-                    )
-        
-        except Exception as e:
-            logging.error(f"Operation do not complete in vuls : {e}")
-    
-    except Exception as e:
-        logging.error(f"Operation do not complete in vuls : {e}")
--- backend/api_applications/banner_grabbing/main.py
+++ backend/api_applications/banner_grabbing/main.py
@@ -1,10 +1,6 @@
 import logging
-import os
-import threading
 
-import schedule
-
-from . import db_operations, vulnerability
+from . import db_operations
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -13,26 +9,7 @@ logging.basicConfig(
 
 def main():
     while True:
-        banners_thread = threading.Thread(
-            target=db_operations.update_banners, name="BannersThread"
-        )
-        vulnerability_thread = threading.Thread(
-            target=db_operations.update_vulnerability, name="VulnerabilityThread"
-        )
-        banners_thread.start()
-        vulnerability_thread.start()
-        logging.info("Waiting for enrichment and banner threads to complete...")
-        banners_thread.join()
-        vulnerability_thread.join()
-        try:
-            schedule.every(1.5).hours.do(
-                vulnerability.download_and_replace_nvd,
-                os.path.join(os.path.dirname(__file__), "cve_data"),
-            )
-        
-        except Exception as e:
-            logging.error("Failed to download and replace CVE file.")
-            
+        db_operations.update_banners()
             
 
 
