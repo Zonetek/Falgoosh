60124d2862d66dbed685cc7077db5c202fe8f388
Mobinamousavii <mobinamsv6@gmail.com>
2025-08-03 15:38:55 +0330

    feat(models): add Ticket and TicketMessage models

--- backend/api_applications/shared_models/models/__init__.py
+++ backend/api_applications/shared_models/models/__init__.py
@@ -1,8 +1,11 @@
 from .user import CustomUser, UserProfile
 from .scan import Scan  
+from .tickets import Ticket, TicketMessage
 
 __all__ = [
     "CustomUser",
     "UserProfile",
     "Scan",
+    "Ticket",
+    "TicketMessage"
 ]
