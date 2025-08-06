7b52b145d30b10d449a2e04dc37da137f085c189
Mobinamousavii <mobinamsv6@gmail.com>
2025-08-06 20:26:59 +0330

    feat(tickets): add user/admin ticket views and ticket_admin group
    - Implemented user ticket viewset for managing user's own tickets
    - Implemented admin ticket viewset with reply and status change actions
    - Updated create_admin_groups command to include ticket_admin group and related permissions

--- backend/api_applications/tickets/views.py
+++ backend/api_applications/tickets/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import get_object_or_404
 
 from api_applications.shared_models.models import Ticket, TicketMessage
 from api_applications.tickets.serializers import TicketCreateSerializer, TicketSerializer, TicketMessageSerializer
+from api_applications.admin_tools.permissions import HasGroup
+
 
 class TicketViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
@@ -34,3 +36,49 @@ class TicketViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save(ticket=ticket, sender=request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class AdminTicketViewSet(viewsets.ModelViewSet):
+
+    queryset = Ticket.objects.all().order_by("-created_at")
+    serializer_class = TicketSerializer
+    permission_classes = [permissions.IsAuthenticated, HasGroup("super_admin")] 
+
+    def get_queryset(self):
+        queryset = self.queryset
+        status_filter = self.request.query_params.get("status")
+        if status_filter:
+            queryset = queryset.filter(status=status_filter)
+        return queryset
+
+    @action(detail=True, methods=["post"])
+    def reply(self, request, pk=None):
+        ticket = self.get_object()
+
+        serializer = TicketMessageSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(ticket=ticket, sender=request.user)
+
+        if ticket.status in ["closed", "pending"]:
+            ticket.status = "reviewing"
+        elif ticket.status == "reviewing":
+            ticket.status = "answered"
+
+        ticket.save(update_fields=["status", "updated_at"])
+
+        return Response({
+            "message": "Reply added successfully",
+            "ticket_status": ticket.status
+        }, status=status.HTTP_201_CREATED)
+
+    @action(detail=True, methods=["post"])
+    def change_status(self, request, pk=None):
+        ticket = self.get_object()
+        new_status = request.data.get("status")
+
+        if new_status not in dict(Ticket.STATUS_CHOICES).keys():
+            return Response({"error": "Invalid status"}, status=status.HTTP_400_BAD_REQUEST)
+
+        ticket.status = new_status
+        ticket.save(update_fields=["status", "updated_at"])
+
+        return Response({"message": f"Status changed to {new_status}"} ,status=status.HTTP_200_OK)
