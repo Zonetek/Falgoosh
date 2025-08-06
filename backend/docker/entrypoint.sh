876e9915957457183de294de2e2c42be2c004807
Amirhossein Dezhabdolahi <mrahd66@gmail.com>
2025-08-06 13:13:34 +0330

    ci: add wait_for_it for rabbitmq

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -6,7 +6,8 @@ set -e
 echo "📦 Running Django setup..."
 
 echo "Waiting for Postgres..."
-/wait-for-it.sh db:5432 --timeout=30 --strict -- echo "Postgres is up"
+/wait-for-it.sh postgres:5432 --timeout=30 --strict -- echo "Postgres is up"
+/wait-for-it.sh rabbitmq:5672 --timeout=20 --strict
 
 # Run migrations
 echo "📄 Running migrations..."
