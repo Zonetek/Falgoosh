4b5b3a8b711026378e2fe41fe631291de6c4559b
Amirhossein Dezhabdolahi <mrahd66@gmail.com>
2025-08-06 14:25:00 +0330

    ci(email-celery): add verification email, celery config

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -74,6 +74,43 @@ INTERNAL_IPS = [
 ]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
+# EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.yourhost.com')
+# EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
+# DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'no-reply@yourdomain.com')
 # Development-specific settings
-ACCOUNT_EMAIL_VERIFICATION = "none"
+
+ACCOUNT_SIGNUP_FIELDS = ['username*', 'email*', 'password1*', 'password2*']
+ACCOUNT_LOGIN_METHODS = {'email'}
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_CONFIRM_EMAIL_ON_GET = False  # This makes the GET verify the user
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = False
+
+# Override URLs in confirmation emails to point to frontend:
+ACCOUNT_CONFIRMATION_EMAIL_TEMPLATE = "account/email/email_confirmation_message.txt"
+
+# Headless support: replace links in email templates with frontend URLs
+HEADLESS_FRONTEND_URLS = {
+  "account_confirm_email": "https://your-frontend.com/verify-email/{key}",
+  "account_signup":      "https://your-frontend.com/signup",
+  "account_reset_password": "https://your-frontend.com/reset-password",
+  "account_reset_password_from_key": "https://your-frontend.com/reset-password-confirm/{key}",
+}
+
+HEADLESS_ONLY = False
+
+CELERY_BROKER_URL = os.getenv('RABBITMQ_URL')
+CELERY_RESULT_BACKEND = 'rpc://'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_BEAT_SCHEDULE = {
+    'run-example-task-every-minute': {
+        'task': 'app.tasks.example_task',
+        'schedule': 60.0,
+        'args': (),
+    },
+}
