ac28614d95bfe69637d97555b2399600d05d4f33
Amirhossein Dezhabdolahi <mrahd66@gmail.com>
2025-08-19 12:08:27 +0330

    feat(user-profile): add user profile admin

--- backend/api_applications/accounts/admin.py
+++ backend/api_applications/accounts/admin.py
@@ -1,1 +1,14 @@
 from django.contrib import admin
+from api_applications.shared_models.models.user import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "is_verified",
+        "last_login_ip",
+        "session_id",
+        "created_at",
+        "updated_at",
+    )
