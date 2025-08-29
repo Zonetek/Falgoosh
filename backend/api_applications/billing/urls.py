1414e7666fe75f05f5d490bb2c158cb1febf8624
Amirhossein Dezhabdolahi <11095582-Amirhosseindzh@users.noreply.gitlab.com>
2025-08-29 11:53:24 +0330

    fix: delete unused scan url

--- backend/api_applications/billing/urls.py
+++ backend/api_applications/billing/urls.py
@@ -2,14 +2,12 @@ from django.urls import path
 from .views import (
     PlanListView,
     CreateInvoiceView,
-    PerformScanView,
     ProviderWebhookView,
 )
 
 urlpatterns = [
     path("plans/", PlanListView.as_view(), name="plans-list"),
     path("invoices/create/", CreateInvoiceView.as_view(), name="invoice-create"),
-    path("scan/", PerformScanView.as_view(), name="perform-scan"),
     path(
         "webhook/<str:provider>/",
         ProviderWebhookView.as_view(),
